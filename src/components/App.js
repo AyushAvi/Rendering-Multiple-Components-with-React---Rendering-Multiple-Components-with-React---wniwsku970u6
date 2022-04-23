48b0d5e8996f6920f5da4039e922ddfb601fe779
AyushAvi <70689163+AyushAvi@users.noreply.github.com>
2022-04-24 02:07:58 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">My Project Topic</h1>
+      <p data-ns-test="project-description">My Project Description</p>
+    </div>
   )
 }
 
